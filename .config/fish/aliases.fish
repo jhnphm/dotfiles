f99ece9d1241b5f2efaa46e5990e3d71f6898fa5
John Pham <jhnphm@gmail.com>
2015-05-19 15:00:40 -0400

    Update vim config and add fish configs

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,5 @@
+#alias j 'jump'
+alias cp 'cp --reflink=auto'
+alias ls 'ls --color=auto'
+alias v "callvim -b':tabnew'"
+alias gstatus "/usr/bin/gvim --remote +'Gstatus' commit"
---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+function fish_user_key_bindings --description 'Custom keybindings'
+    # Clear earlier bindings, if any
+    bind \ee edit_cmd
+end
+
