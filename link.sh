81d1c0c17162656be9ae06638a11674144a4fc7f
John Pham <jhnphm@gmail.com>
2015-05-19 18:27:42 -0400

    Update link script and gitignore

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
-#! /bin/zsh
+#! /bin/sh
 DOTFILES=`pwd`
 
-ln -sf $DOTFILES/.vim* ..
-ln -sf $DOTFILES/.zsh* ..
-ln -sf $DOTFILES/.xprofile ..
+ln -sf $DOTFILES/.vim* ~
+ln -sf $DOTFILES/.zsh* ~
+ln -sf $DOTFILES/.oh-my-* ~
+ln -sf $DOTFILES/.config/fish ~/.config
+ln -sf $DOTFILES/.xprofile ~
